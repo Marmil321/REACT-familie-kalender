ff501026000e18e141ba41e887e1b5568774f741
Marcus Milsem <47544415+Marmil321@users.noreply.github.com>
2025-06-26 13:03:47 +0200

    Add familyMembers utility with member data
    Introduces a new utility file exporting an array of family member objects, each containing name, id, and color properties for use in the application.

---
+++ familie-kalender/src/app/(frontend)/main/utility/familyMembers.ts
@@ -0,0 +1,39 @@
+// utility/familyMembers.ts
+
+export const familyMembers = [
+  {
+    name: 'Marcus',
+    id: 'marcus',
+    color: '#DAF7A6', //green
+  },
+  {
+    name: 'Lars',
+    id: 'lars',
+    color: '#7DF9FF',//blue
+  },
+  {
+    name: 'Lucas',
+    id: 'lucas',
+    color: '#FFC300',//orange
+  },
+  {
+    name: 'Noomi',
+    id: 'noomi',
+    color: '#8e44ad',//purple
+  },
+  {
+    name: 'Meline',
+    id: 'meline',
+    color: '#e74c3c',//red
+  },
+  {
+    name: 'Bailey',
+    id: 'bailey',
+    color: '#FF33A1',//pink
+  },
+  {
+    name: 'Marita',
+    id: 'marita',
+    color: '#f4d03f',//yellow
+  }
+];
